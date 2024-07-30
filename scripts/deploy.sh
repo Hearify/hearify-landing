38961142c39d23bf598b4fba8e5d5fe5b6622577
Oleksandr Humeniuk <oleksandrhumeniuk2@gmail.com>
2024-07-30 11:51:28 +0300

    chore: test workflow

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop and remove existing container if running
+existing_container_id=$(docker ps -q --filter "name=blog-nextjs")
+if [ ! -z "$existing_container_id" ]; then
+  docker stop $existing_container_id
+  docker rm $existing_container_id
+fi
+
+# Remove any existing image to avoid conflicts
+existing_image_id=$(docker images -q blog-nextjs)
+if [ ! -z "$existing_image_id" ]; then
+  docker rmi $existing_image_id
+fi
+
+# Build and run new Docker container
+docker build -t blog-nextjs .
+docker run -d -p 3000:3000 --name blog-nextjs blog-nextjs
