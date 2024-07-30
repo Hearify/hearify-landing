85d2f9e6c8d26a89b34f7985cb65d061ca32e052
Oleksandr Humeniuk <oleksandrhumeniuk2@gmail.com>
2024-07-30 17:38:39 +0300

    chore: add hotjar scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -94,6 +94,22 @@ const App = ({ Component, pageProps }: AppProps) => {
         }}
       />
 
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+              <script>
+                  (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:4934986,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              </script>`,
+        }}
+      />
+
       <div className={`${nunito.variable} ${caveat.variable}`}>
         <PageLayout>
           <Component {...pageProps} />
