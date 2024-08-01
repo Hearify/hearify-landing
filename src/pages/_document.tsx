b50550c605e3349de97c67d9eb89f9876204ab91
Oleksandr Humeniuk <oleksandrhumeniuk2@gmail.com>
2024-08-01 11:06:22 +0300

    chore: add google tag managers script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,21 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            title="Google Tag Manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PV5D2MP9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
         <Main />
         <NextScript />
       </body>
