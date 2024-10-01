c86283ddcf7a45169d591ff7e2adcd4c9bc501dd
oleksandr <oleksandrhumeniuk2@gmail.com>
2024-10-01 15:58:08 +0300

    fix: change create quiz images

--- src/templates/Home/HomeCreateQuiz/HomeCreateQuiz.tsx
+++ src/templates/Home/HomeCreateQuiz/HomeCreateQuiz.tsx
@@ -11,6 +11,11 @@ import createQuizImage2 from '@/assets/images/create-quiz/create-quiz-2.png';
 import createQuizImage3 from '@/assets/images/create-quiz/create-quiz-3.png';
 import createQuizImage4 from '@/assets/images/create-quiz/create-quiz-4.png';
 import createQuizImage5 from '@/assets/images/create-quiz/create-quiz-5.png';
+import mobileCreateQuizImage1 from '@/assets/images/mobile-create-quiz/mobile-create-quiz-1.png';
+import mobileCreateQuizImage2 from '@/assets/images/mobile-create-quiz/mobile-create-quiz-2.png';
+import mobileCreateQuizImage3 from '@/assets/images/mobile-create-quiz/mobile-create-quiz-3.png';
+import mobileCreateQuizImage4 from '@/assets/images/mobile-create-quiz/mobile-create-quiz-4.png';
+import mobileCreateQuizImage5 from '@/assets/images/mobile-create-quiz/mobile-create-quiz-5.png';
 import useDeviceDetect from '@/hooks/useDeviceDetect';
 import styles from './HomeCreateQuiz.module.scss';
 
@@ -63,13 +68,12 @@ const HomeCreateQuiz: React.FC = () => {
           <div className={styles.body}>
             <h4 className={styles.subtitle}>{currentTitle}</h4>
             <p className={styles.description}>{currentDescription}</p>
-
             <Link href="https://app.hearify.org" className={styles.images}>
-              {activeStep === 0 && <Image src={createQuizImage1} alt={currentTitle} width={500} height={500} />}
-              {activeStep === 1 && <Image src={createQuizImage2} alt={currentTitle} width={500} height={500} />}
-              {activeStep === 2 && <Image src={createQuizImage3} alt={currentTitle} width={500} height={500} />}
-              {activeStep === 3 && <Image src={createQuizImage4} alt={currentTitle} width={500} height={500} />}
-              {activeStep === 4 && <Image src={createQuizImage5} alt={currentTitle} width={500} height={500} />}
+              {activeStep === 0 && <Image src={createQuizImage1} alt={currentTitle} width={350} height={400} />}
+              {activeStep === 1 && <Image src={createQuizImage2} alt={currentTitle} width={450} height={400} />}
+              {activeStep === 2 && <Image src={createQuizImage3} alt={currentTitle} width={450} height={400} />}
+              {activeStep === 3 && <Image src={createQuizImage4} alt={currentTitle} width={400} height={300} />}
+              {activeStep === 4 && <Image src={createQuizImage5} alt={currentTitle} width={750} height={400} />}
             </Link>
           </div>
         </div>
@@ -92,14 +96,13 @@ const HomeCreateQuiz: React.FC = () => {
                   {index + 1}. {item.title}
                 </h4>
                 <p className={styles.description}>{item.description}</p>
-
-                <div className={styles.images}>
-                  {index === 0 && <Image src={createQuizImage1} alt={item.title} width={500} height={500} />}
-                  {index === 1 && <Image src={createQuizImage2} alt={item.title} width={500} height={500} />}
-                  {index === 2 && <Image src={createQuizImage3} alt={item.title} width={500} height={500} />}
-                  {index === 3 && <Image src={createQuizImage4} alt={item.title} width={500} height={500} />}
-                  {index === 4 && <Image src={createQuizImage5} alt={item.title} width={500} height={500} />}
-                </div>
+                <Link href="https://app.hearify.org" className={styles.images}>
+                  {index === 0 && <Image src={mobileCreateQuizImage1} alt={currentTitle} width={350} height={400} />}
+                  {index === 1 && <Image src={mobileCreateQuizImage2} alt={currentTitle} width={450} height={400} />}
+                  {index === 2 && <Image src={mobileCreateQuizImage3} alt={currentTitle} width={450} height={400} />}
+                  {index === 3 && <Image src={mobileCreateQuizImage4} alt={currentTitle} width={400} height={300} />}
+                  {index === 4 && <Image src={mobileCreateQuizImage5} alt={currentTitle} width={750} height={400} />}
+                </Link>
               </div>
             ))}
           </Carousel>
