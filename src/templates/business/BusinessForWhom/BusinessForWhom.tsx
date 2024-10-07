599981276b9b89a28b4a1d5484b378982be0b032
Maxim Lemish <maximka.lemish@gmail.com>
2024-10-07 19:39:33 +0300

    feat: add Business-For-Whom container(not finished)

---
+++ src/templates/business/BusinessForWhom/BusinessForWhom.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './BusinessForWhom.module.scss';
+
+const BusinessForWhom = () => {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>For Whom</h1>
+        <div className={styles.cardsWrapper}>
+          <div className={styles.card}>lalalala</div>
+          <div className={styles.card}>lalalala</div>
+          <div className={styles.card}>lalalala</div>
+          <div className={styles.card}>lalalala</div>
+          <div className={styles.card}>lalalala</div>
+          <div className={styles.card}>lalalala</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BusinessForWhom;
