abbf4a1b31cd124668f99076bf61ae6e88bfee39
Maxim Lemish <maximka.lemish@gmail.com>
2024-10-07 19:32:28 +0300

    feat: add Business landing page

--- src/templates/business/Business.tsx
+++ src/templates/business/Business.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 import styles from './Business.module.scss';
 import BusinessHero from './BusinessHero/BusinessHero';
+import BusinessForWhom from './BusinessForWhom/BusinessForWhom';
+import BusinessBoostAlert from './BusinessBoostAlert/BusinessBoostAlert';
+import BusinessInfoBoard from './BusinessInfoBoard/BusinessInfoBoard';
 
-const B2B: React.FC = () => {
+const Business: React.FC = () => {
   return (
     <main className={styles.wrapper}>
       <BusinessHero />
+      <BusinessForWhom />
+      <BusinessInfoBoard />
+      <BusinessBoostAlert />
     </main>
   );
 };
 
-export default B2B;
+export default Business;
