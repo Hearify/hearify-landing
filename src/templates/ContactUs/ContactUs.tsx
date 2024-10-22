0a9b07f4dccb274bc37b460db7f21388e8abcb74
Maxim Lemish <maximka.lemish@gmail.com>
2024-10-22 18:32:39 +0300

    feat: add Contact Us page

---
+++ src/templates/ContactUs/ContactUs.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useTranslation } from 'next-i18next';
+
+import AboutUsBoostAlert from '@/containers/AboutUsBoostAlert/AboutUsBoostAlert';
+import styles from './ContactUs.module.scss';
+
+type LinkType = {
+  name: string;
+  link: string;
+};
+
+type DataType = {
+  title: string;
+  description: string;
+  links: LinkType[];
+};
+
+const ContactUs: React.FC = () => {
+  const { t } = useTranslation('common', { keyPrefix: 'templates.ContactUs' });
+  const data: DataType[] = t('data', { returnObjects: true });
+
+  return (
+    <main className={styles.wrapper}>
+      <h1 className={styles.title}>{t('title')}</h1>
+      <p className={styles.description}>{t('description')}</p>
+      <ul className={styles.list}>
+        {data.map(item => (
+          <li key={item.title} className={styles.item}>
+            <h2 className={styles.itemTitle}>{item.title}</h2>
+            <p className={styles.itemDescription}>{item.description}</p>
+            <div className={styles.link}>
+              {item.links.map(link => (
+                <a key={link.name} href={link.link}>
+                  {link.name}
+                </a>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <AboutUsBoostAlert />
+    </main>
+  );
+};
+
+export default ContactUs;
