9a05042bcaf313ebdf663f86e48ebf63f63829bb
Teasotea <sofi16616@gmail.com>
2025-02-15 06:41:48 +0200

    Added tablet adaptation

--- src/containers/ForWhomCard/ForWhomCard.tsx
+++ src/containers/ForWhomCard/ForWhomCard.tsx
@@ -19,7 +19,7 @@ const ForWhomCard: React.FC<ForWhomCardProps> = ({
   return (
     <Link href={link} className={styles.wrapper}>
       <div className={styles.imageWrapper}>
-        <ImageComponent className={styles.image} width={56} height={56} />
+        <ImageComponent className={styles.image} width={56} height={56} color="#AC9AFF" />
       </div>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
