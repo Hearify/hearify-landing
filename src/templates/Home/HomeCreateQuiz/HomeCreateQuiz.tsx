3f494244aa2cb8272e734bbff79f8705a0eed7e0
Sofi Shaposhnikova <73386821+Teasotea@users.noreply.github.com>
2025-05-31 18:32:55 +0300

    Update HomeCreateQuiz.tsx

--- src/templates/Home/HomeCreateQuiz/HomeCreateQuiz.tsx
+++ src/templates/Home/HomeCreateQuiz/HomeCreateQuiz.tsx
@@ -12,9 +12,24 @@ const HomeCreateQuiz: React.FC = () => {
       <AppHeading variant="h2" className={styles.title}>
         {t('title')}
       </AppHeading>
-
+      
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video src="/how_it_works.mp4" width="100%" controls />
+      {/* <video src="/how_it_works.mp4" width="100%" controls /> */}
+
+      <div style={{ position: 'relative', paddingBottom: '62.5%', height: 0 }}>
+        <iframe
+          src="https://www.loom.com/embed/06e96bcd3b2e4a90b0a884e54d905d51?sid=69b1638d-4a2f-4983-ad39-67094c476f70"
+          frameBorder="0"
+          allowFullScreen
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </div>
     </section>
   );
 };
